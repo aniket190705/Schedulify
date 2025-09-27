ce10601b3f91814071f849f028e51d7add02e7a6
Anushree Jaiswal <anushree416jaiswal@gmail.com>
2025-09-27 13:43:53 +0530

    added  new ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,26 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import App from "./App";
-import AdminInputForm from "./Components/AdminInputForm";
+
+import FrontPage from "./Components/FrontPage";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route
-          path="/admin"
-          element={
-            <AdminInputForm
-              onSubmit={(data) => {
-                console.log("Admin Input Data:", data);
-                // You can add navigation or API calls here after submit
-              }}
-            />
-          }
-        />
+        <Route path="/" element={<FrontPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
